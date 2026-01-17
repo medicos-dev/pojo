67a7c38881910aa95efe7e4894a78c2bbd7f59c8
medicos-dev <medicosmedlife24naskar@gmail.com>
2026-01-17 10:49:45 +0530

    gg

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // WebRTC Configuration
 const ICE_SERVERS = [
-    { urls: "stun:stun.l.google.com:19302" },
-    { urls: "stun:stun1.l.google.com:19302" },
+    // { urls: "stun:stun.l.google.com:19302" },
+    // { urls: "stun:stun1.l.google.com:19302" },
     { urls: "stun:stun.cloudflare.com:443" },
 
 ];
